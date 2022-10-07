21434f893263052f77744ebff0f06272d62d09ef
mMomarMm <omarzc1609@gmail.com>
2022-10-07 17:59:20 -0500

    Main menu basic, lading scene script, and textmesh pro

---
+++ Virus01/Assets/Scripts/Mainmenu.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Mainmenu : MonoBehaviour
+{
+    public void NextScene()
+    {
+        //SceneLoader.nextScene = 2;
+        SceneManager.LoadScene(1);
+    }
+    public void Quit() { Application.Quit(); }
+    public void Volume(int vol)
+    {
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
