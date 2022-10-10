87c13fb5254c775ac3eacc3febb1bb33fd358c07
aurorusho <87437789+aurorusho@users.noreply.github.com>
2022-10-10 07:44:49 -1200

    Made keys appear correctly

--- Virus01/Assets/Scripts/ButtonScript.cs
+++ Virus01/Assets/Scripts/ButtonScript.cs
@@ -8,10 +8,21 @@ using TMPro;
 public class ButtonScript : MonoBehaviour
 {
     private string ABC = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    float spaceBetween = 1.2f;
+
+    public GameObject container;
+
+    public GameObject referenceObject;
+    public GameObject referenceFinalObject;
+    
+    // Key game object
     public GameObject UIPrefab;
 
+    // Button game objects
     public GameObject attackButton, defenseButton, dodgeButton;
 
+    // Spites
     private Sprite attackSprite, defenseSprite, dodgeSprite;
     public Sprite attackOnClickSprite, defenseOnClickSprite, dodgeOnClickSprite;
 
@@ -34,16 +45,24 @@ public class ButtonScript : MonoBehaviour
     }
     public void SelectAction(int objectId)
     {
+        for (int i = 0; i < container.transform.childCount; i++)
+        {
+            Destroy(container.transform.GetChild(i).gameObject);
+        }
         switch (objectId)
         {
             // Attack
-            case 0: OnAttack();
+            case 0: 
+
+                OnAttack();
                 break;
             // Defense
-            case 1: OnDefense();
+            case 1:
+                OnDefense();
                 break;
             // Dodge
-            case 2: OnDodge();
+            case 2:
+                OnDodge();
                 break;
         }
     }
@@ -66,24 +85,35 @@ public class ButtonScript : MonoBehaviour
     }
     private void OnDefense()
     {
-
+        ChangeImage(defenseButton, defenseOnClickSprite);
+        StartCoroutine(ResetImage(defenseButton, defenseSprite));
+        CreateKeys(6, 8);
     }
     private void OnDodge()
     {
-
+        ChangeImage(dodgeButton, dodgeOnClickSprite);
+        StartCoroutine(ResetImage(dodgeButton, dodgeSprite));
+        CreateKeys(2, 2);
     }
     private void CreateKeys(int minAmount, int maxAmount)
     {
         int keyAmount = UnityEngine.Random.Range(minAmount, maxAmount);
         for(int i = 0; i < keyAmount; i++)
         {
-            GameObject newInstance = Instantiate(UIPrefab, transform);
+            GameObject newInstance = Instantiate(UIPrefab, container.transform);
+            newInstance.transform.position = new Vector2(MoveKey(keyAmount, i), 0);
+
             if (newInstance.transform.GetChild(0).TryGetComponent(out TMP_Text u))
             {
                 u.text = ABC[UnityEngine.Random.Range(0, 25)].ToString();
-                u.transform.position = new Vector3(i, 0, 0);
             }
         }
+    }
+    private float MoveKey(int n, int i)
+    {        
+        float containerWidth = referenceFinalObject.transform.position.x;
+        float xPosition = (containerWidth / 16) + (spaceBetween * (i - (n / 2)));
+        return xPosition;
     }
 
     
