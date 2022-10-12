080683f25a4af837f0be8a43032039dc5d824e7d
aurorusho <87437789+aurorusho@users.noreply.github.com>
2022-10-12 05:45:55 -1200

    Fixed key feature and made cleaner code

--- Virus01/Assets/Scripts/ButtonScript.cs
+++ Virus01/Assets/Scripts/ButtonScript.cs
@@ -21,11 +21,15 @@ public class ButtonScript : MonoBehaviour
 
     // Button game objects
     public GameObject attackButton, defenseButton, dodgeButton;
+    private Dictionary<int, List<GameObject>> commandDict = new Dictionary<int, List<GameObject>>();
 
-    // Spites
+    // Sprites
     private Sprite attackSprite, defenseSprite, dodgeSprite;
     public Sprite attackOnClickSprite, defenseOnClickSprite, dodgeOnClickSprite;
 
+    private Dictionary<int, Dictionary<string, object>> dataDict;
+
+    private int lastClicked = 10;
 
     // Start is called before the first frame update
     void Start() {
@@ -36,6 +40,41 @@ public class ButtonScript : MonoBehaviour
         attackSprite = attackImg.sprite;
         defenseSprite = defenseImg.sprite;
         dodgeSprite = dodgeImg.sprite;
+
+        for(sbyte i = 0; i < 3; i++) commandDict.Add(i, new List<GameObject>());
+
+        dataDict = new Dictionary<int, Dictionary<string, object>>{
+            {
+                0, new Dictionary<string, object>
+                {
+                    {"minAmount", 10 },
+                    {"maxAmount", 16 },
+                    {"objectSprite", attackSprite },
+                    {"onclickSprite", attackOnClickSprite },
+                    {"objectButton", attackButton}
+                }
+            },
+            {
+                1, new Dictionary<string, object>
+                {
+                    {"minAmount", 6 },
+                    {"maxAmount", 8 },
+                    {"objectSprite", defenseSprite },
+                    {"onclickSprite", defenseOnClickSprite },
+                    {"objectButton", defenseButton}
+                }
+            },
+            {
+                2, new Dictionary<string, object>
+                {
+                    {"minAmount", 2 },
+                    {"maxAmount", 2 },
+                    {"objectSprite", dodgeSprite },
+                    {"onclickSprite", dodgeOnClickSprite },
+                    {"objectButton", dodgeButton}
+                }
+            },
+        };
     }
 
     // Update is called once per frame
@@ -45,26 +84,39 @@ public class ButtonScript : MonoBehaviour
     }
     public void SelectAction(int objectId)
     {
-        for (int i = 0; i < container.transform.childCount; i++)
+        if(lastClicked == objectId)
         {
-            Destroy(container.transform.GetChild(i).gameObject);
+            return;
         }
-        switch (objectId)
-        {
-            // Attack
-            case 0: 
+        lastClicked = objectId;
+        
+        Dictionary<string, object> data = dataDict[objectId];
+        int minAmount = (int)data["minAmount"];
+        int maxAmount = (int)data["maxAmount"];
+        Sprite objectSprite = (Sprite)data["objectSprite"];
+        Sprite onclickSprite = (Sprite)data["onclickSprite"];
+        GameObject buttonObject = (GameObject)data["objectButton"];
 
-                OnAttack();
-                break;
-            // Defense
-            case 1:
-                OnDefense();
-                break;
-            // Dodge
-            case 2:
-                OnDodge();
-                break;
+        ChangeImage(buttonObject, onclickSprite);
+        StartCoroutine(ResetImage(buttonObject, objectSprite));
+
+        DestroyObjects();
+
+        List<GameObject> attackList = commandDict[objectId];
+
+        int length = attackList.Count;
+        print(length);
+        if (length > 0)
+        {
+            foreach (GameObject attack in attackList)
+            {
+                attack.SetActive(true);
+            }
+            return;
         }
+        // else
+        CreateKeys(objectId, minAmount, maxAmount);
+
     }
     private IEnumerator ResetImage(GameObject imgObject, Sprite newSprite)
     {
@@ -76,27 +128,9 @@ public class ButtonScript : MonoBehaviour
         imgObject.TryGetComponent(out Image image);
         image.sprite = newSprite;
     }
-    private void OnAttack()
+    private void CreateKeys(int objectId, int minAmount, int maxAmount)
     {
-        ChangeImage(attackButton, attackOnClickSprite);
-        StartCoroutine(ResetImage(attackButton, attackSprite));
-        CreateKeys(10, 16);
-
-    }
-    private void OnDefense()
-    {
-        ChangeImage(defenseButton, defenseOnClickSprite);
-        StartCoroutine(ResetImage(defenseButton, defenseSprite));
-        CreateKeys(6, 8);
-    }
-    private void OnDodge()
-    {
-        ChangeImage(dodgeButton, dodgeOnClickSprite);
-        StartCoroutine(ResetImage(dodgeButton, dodgeSprite));
-        CreateKeys(2, 2);
-    }
-    private void CreateKeys(int minAmount, int maxAmount)
-    {
+        DestroyObjects();
         int keyAmount = UnityEngine.Random.Range(minAmount, maxAmount);
         for(int i = 0; i < keyAmount; i++)
         {
@@ -105,8 +139,10 @@ public class ButtonScript : MonoBehaviour
 
             if (newInstance.transform.GetChild(0).TryGetComponent(out TMP_Text u))
             {
-                u.text = ABC[UnityEngine.Random.Range(0, 25)].ToString();
+                string keyStr = ABC[UnityEngine.Random.Range(0, 25)].ToString();
+                u.text = keyStr;
             }
+            commandDict[objectId].Add(newInstance);
         }
     }
     private float MoveKey(int n, int i)
@@ -116,5 +152,11 @@ public class ButtonScript : MonoBehaviour
         return xPosition;
     }
 
-    
+    public void DestroyObjects()
+    {
+        for (int i = 0; i < container.transform.childCount; i++)
+        {
+            container.transform.GetChild(i).gameObject.SetActive(false);
+        }
+    }
 }
